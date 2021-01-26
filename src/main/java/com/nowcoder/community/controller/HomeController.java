35d1f47035b23f3af07b1aaab73e49382e10d7b0
ss <603904153@qq.com>
2021-01-26 10:51:14 +0800

    首次提交代码

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -43,6 +43,7 @@ public class HomeController {
                 discussPosts.add(map);
             }
         }
+        //
         model.addAttribute("discussPosts", discussPosts);
         return "index";
     }
